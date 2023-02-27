76bddd2e8773f91ceb91594a3786d740b5633ea7
NathaelB <kirua48000@gmail.com>
2023-02-27 18:27:36 +0100

    :construction: work

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,35 @@
 import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
+import User from '../../Models/User'
+import {StoreValidator, UpdateValidator} from "App/Validators/UserValidator";
 
 export default class UsersController {
-  public async index ({}: HttpContextContract) {}
-  public async show ({}: HttpContextContract) {}
-  public async store ({}: HttpContextContract) {}
-  public async update ({}: HttpContextContract) {}
-  public async delete ({}: HttpContextContract) {}
+  public async index (): Promise<User[]> {
+    return User.query()
+  }
+
+  public async show ({ params }: HttpContextContract): Promise<User | null> {
+    return User.query()
+      .where('id', params.id)
+      .preload('roles', (query) => {
+        query.preload('permissions')
+      })
+      .preload('permissions')
+      .first()
+  }
+
+  public async store ({ request }: HttpContextContract): Promise<User> {
+    const data = await request.validate(StoreValidator)
+    return User.create(data)
+  }
+
+  public async update ({ params, request }: HttpContextContract) {
+    const data = await request.validate(UpdateValidator)
+    const user = await User.findOrFail(params.id)
+
+  }
+
+  public async delete ({ params }: HttpContextContract): Promise<void> {
+    const user = await User.findOrFail(params.id)
+    return user.delete()
+  }
 }
