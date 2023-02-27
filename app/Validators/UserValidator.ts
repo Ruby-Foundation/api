8e0a11c5304bc020a3840c7f96ab6e666e3d902b
NathaelB <kirua48000@gmail.com>
2023-02-27 18:27:18 +0100

    :construction: create validator & add column for `users`

---
+++ app/Validators/UserValidator.ts
@@ -0,0 +1,50 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({ trim: true }, [
+      rules.unique({
+        table: 'users',
+        column: 'email'
+      })
+    ]),
+    username: schema.string({ trim: true }),
+    password: schema.string({ trim: true }, [
+      rules.minLength(7)
+    ]),
+    password_confirmation: schema.string({ trim: true }, [
+      rules.equalTo('password')
+    ]),
+    roles: schema.array.optional().members(schema.string({ trim: true })),
+    permissions: schema.array.optional().members(schema.string({ trim: true }))
+  })
+
+  public messages: CustomMessages = {}
+}
+
+export class UpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string.optional({ trim: true }, [
+      rules.unique({
+        table: 'users',
+        column: 'email'
+      })
+    ]),
+    username: schema.string.optional({ trim: true }),
+    password: schema.string.optional({ trim: true }, [
+      rules.minLength(7)
+    ]),
+    password_confirmation: schema.string.optional({ trim: true }, [
+      rules.equalTo('password')
+    ]),
+    roles: schema.array.optional().members(schema.string({ trim: true })),
+    permissions: schema.array.optional().members(schema.string({ trim: true }))
+  })
+
+  public messages: CustomMessages = {}
+}
