33ba278697dce93cfb85c5a11e8fa1ee9d6bc14c
NathaelB <kirua48000@gmail.com>
2023-02-28 11:21:39 +0100

    :construction: add and update routes

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,7 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 const authRoute = () => Route.group(() => {
+  Route.post('/login', 'AuthController.login')
+  Route.post('/register', 'AuthController.register')
 
+  Route.group(() => {
+    Route.delete('/logout', 'AuthController.logout')
+    Route.get('/me', 'AuthController.me')
+  }).middleware('auth')
 }).prefix('/auth')
 
 export default authRoute
