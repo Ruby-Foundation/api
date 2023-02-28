1635c8e608e27eeb7aa4bfba1a64bbe1bcb56aa4
NathaelB <kirua48000@gmail.com>
2023-02-28 14:13:32 +0100

    :construction: create model `Ticket`

---
+++ app/Models/Ticket.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import {BaseModel, beforeCreate, BelongsTo, belongsTo, column} from '@ioc:Adonis/Lucid/Orm'
+import {randomUUID} from "crypto";
+import Discord from "App/Models/Discord";
+
+export default class Ticket extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public type: string
+
+  @column()
+  public userId: string
+  
+  @column()
+  public channelId: string
+  
+  @belongsTo(() => Discord)
+  public user: BelongsTo<typeof Discord>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static async generateUuid (ticket: Ticket) {
+    ticket.id = randomUUID()
+  }
+}
